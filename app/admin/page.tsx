8acde0f276a3303b766db4e15e8060d25b70919f
PutluruAravindaReddy <aravindareddy60@gmail.com>
2024-07-10 13:53:57 +0530

    Admin Panel Updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -54,7 +54,7 @@ const Page = () => {
             <p className="text-lg font-semibold mb-4">Welcome, {session?.user?.name}!</p>
             <button
               onClick={() => signOut()}
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              className="px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-600"
             >
               Logout
             </button>
